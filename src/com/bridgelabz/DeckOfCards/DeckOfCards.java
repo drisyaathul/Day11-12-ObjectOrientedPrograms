12db76cd18c977d4eed55b1b78135afba3ef4799
drisyaathul <drisyak809@gmail.com>
2023-04-23 22:45:06 +0800

    Object

---
+++ src/com/bridgelabz/DeckOfCards/DeckOfCards.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.DeckOfCards;
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class DeckOfCards {
+//    private List<Cards> cards;
+//
+//    public DeckOfCards() {
+//        String[] suits = ("Clubs", "Diamonds", "Hearts", "Spades");
+//        String[] ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
+//
+//        ArrayList<Cards> cards = new ArrayList<>();
+//        for (String suit : suits) {
+//            for (String rank : ranks) {
+//                cards.add(new Cards(suit, rank));
+//            }
+//        }
+//    }
+//
+//    public void shuffle() {
+//        Collections.shuffle(cards);
+//    }
+//
+//    public static void main(String[] args) {
+//        DeckOfCards deckOfCards = new DeckOfCards();
+//        deckOfCards.shuffle();
+//
+//        String[] players = {"Player1, "Player2", "Player3", "Player4"}
+//
+//        }
+//    }
+//}
